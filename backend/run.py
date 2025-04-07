608e2456241b4fbf73bf923b05098d2f215343c9
aaronsabellek <aaronsabellek@gmx.de>
2025-04-07 14:06:09 +0200

    Update debug in run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,9 @@
 from app import create_app
 
+# Create app instance
 app = create_app()
 
+# Enable 'debug' in development mode
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config['DEBUG'])
+
