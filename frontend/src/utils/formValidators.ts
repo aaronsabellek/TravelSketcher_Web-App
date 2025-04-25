aa4206eaa17def9d817547bd892d790571e1eb63
aaronsabellek <aaronsabellek@gmx.de>
2025-04-25 16:12:39 +0200

    Minor fixes

--- frontend/src/utils/formValidators.ts
+++ frontend/src/utils/formValidators.ts
@@ -1,33 +1,33 @@
 export const validateNotEmpty = (value: string): string | null =>
-    value.trim() === '' ? 'This field must not be empty.' : null;
+  value.trim() === '' ? 'This field must not be empty.' : null;
 
-  export const validateUsername = (username: string): string | null =>
-    username.includes('@') ? 'Username must not contain "@".' : null;
+export const validateUsername = (username: string): string | null =>
+  username.includes('@') ? 'Username must not contain "@".' : null;
 
-  export const validateEmailFormat = (email: string): string | null =>
-    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) ? null : 'Wrong email format.';
+export const validateEmailFormat = (email: string): string | null =>
+  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) ? null : 'Wrong email format.';
 
   // Check if password matches requirements
 export function validatePasswordRules(password: string): string | null {
-    if (password.length < 8) {
-      return 'Password must be at least 8 characters long.';
-    }
-
-    if (!/[a-zA-Z]/.test(password)) {
-      return 'Password must contain at least one letter.';
-    }
-
-    if (!/[0-9]/.test(password)) {
-      return 'Password must contain at least one number.';
-    }
-
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-      return 'Password must contain at least one special character.';
-    }
-
-    return null;
+  if (password.length < 8) {
+    return 'Password must be at least 8 characters long.';
   }
 
-  export const validatePasswordsMatch = (pw1: string, pw2: string): string | null =>
-    pw1 === pw2 ? null : 'Oasswods do not match.';
+  if (!/[a-zA-Z]/.test(password)) {
+    return 'Password must contain at least one letter.';
+  }
+
+  if (!/[0-9]/.test(password)) {
+    return 'Password must contain at least one number.';
+  }
+
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    return 'Password must contain at least one special character.';
+  }
+
+  return null;
+}
+
+export const validatePasswordsMatch = (pw1: string, pw2: string): string | null =>
+  pw1 === pw2 ? null : 'Oasswods do not match.';
 
--- frontend/src/utils/getValidationErrors.ts
+++
@@ -1,4 +0,0 @@
-export const getValidationErrors = (value: string, validators: ((v: string) => string | null)[]): string[] =>
-    validators
-      .map(fn => fn(value))
-      .filter((e): e is string => e !== null);
