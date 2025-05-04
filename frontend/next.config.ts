521260a383ec4fe75657c27920cc3aa06cb9bfac
aaronsabellek <aaronsabellek@gmx.de>
2025-05-04 13:11:03 +0200

    Changes to host frontend as web service

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',  // Stellt den statischen Export ein
-  images: {
-    unoptimized: true, // Optional, falls du externe Bilder ohne Optimierung verwenden möchtest
-  },
 };
 
 export default nextConfig;
