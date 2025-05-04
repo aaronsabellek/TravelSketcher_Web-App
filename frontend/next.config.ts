a3aabcba0e9a45aeb79bddc317db0f930fdd252e
aaronsabellek <aaronsabellek@gmx.de>
2025-05-04 12:18:36 +0200

    Changes for static frontend deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // Stellt den statischen Export ein
+  images: {
+    unoptimized: true, // Optional, falls du externe Bilder ohne Optimierung verwenden möchtest
+  },
 };
 
 export default nextConfig;
